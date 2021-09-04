469c482b177be333a501b3b7cf64aede930e8437
Damon German <damon.german@hyland.com>
2021-09-04 14:50:41 -0500

    made some changes

---
+++ PrePostCondition/PrePostCondition/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrePostCondition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new SquareRootDerived();
+            var ff = new SquareRootBase();
+            ff.SquareRoot(1);
+            var res = sr.SquareRoot(5.0);
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ PrePostCondition/PrePostCondition/SquareRootBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PrePostCondition
+{
+    public class SquareRootBase
+    {
+        public virtual double SquareRoot(double x)
+        {
+            if (x < 0) throw new Exception(); // precondition
+
+            var y = Math.Sqrt(x);
+
+            if (Math.Abs((y * y) - x) > 0.00000001) throw new Exception(); // postcondition
+
+            return y;
+        }
+    }
+
+    public class SquareRootDerived : SquareRootBase
+    {
+        public override double SquareRoot(double x)
+        {
+            if (x is < 0 and < 10) throw new Exception(); // precondition stronger violates the LSP
+
+            var y = Math.Sqrt(x);
+
+            if (Math.Abs((y * y) - x) > 0.1) throw new Exception(); // postcondition weaker violates the LSP
+
+            return y;
+        }
+    }
+}
